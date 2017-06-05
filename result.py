247dd86ac3fb4ca69134629ef6e2e80905e63d44
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-06-05 22:51:47 +0100

    correct printing assignment matrix

--- result.py
+++ result.py
@@ -7,14 +7,20 @@ import numpy as np
 classes = ["apple", "pen", "book", "monitor", "mouse", "wallet", "keyboard",
            "banana", "key", "mug", "pear", "orange"]
 
-ass_tab = np.load("assignments_table.npy")
+ass_tab = np.load("good_ass_tab.npy")
 
 for i in range(12):
     print("%8s" % classes[i], end=' ')
-    print()
-    for i in range(12):
-        for j in range(12):
-            print("%8.2f " % ass_tab[i, j], end='')
+
+print()
+
+for i in range(12):
+    for j in range(12):
+        print("%8.2f " % ass_tab[i, j], end='')
     print()
 
 
+# idx_wrong = np.load("idx_guess_wrong.npy")
+#
+# for idx in idx_wrong:
+#     print(idx)
