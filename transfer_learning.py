c5eb996e6d08e0c3ed72ac2d4a1ee2fdb654539b
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-06-12 21:24:33 +0100

    trasnfer learning using Keras

---
+++ transfer_learning.py
@@ -0,0 +1,146 @@
+import keras
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+import numpy as np
+
+# NOTE THAT classes should be in sorted order because generator KERAS will do so if we don't specify classes
+classes = ['apple', 'banana', 'book', 'key', 'keyboard', 'monitor', 'mouse', 'mug', 'orange', 'pear', 'pen', 'wallet']
+num_classes = len(classes)
+img_width, img_height = 224, 224
+
+train_data_dir = 'data/train'
+val_data_dir = 'data/val'
+test_data_dir = 'data/test'
+
+data_augmentation = False
+
+NB_TRAIN_PER_CLASS = 720
+NB_VAL_PER_CLASS = 180
+NB_TEST_PER_CLASS = 300
+
+nb_train_samples = NB_TRAIN_PER_CLASS * num_classes
+nb_validation_samples = NB_VAL_PER_CLASS * num_classes
+epochs = 2
+batch_size = 48
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+def simple_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+
+    adam_opt = keras.optimizers.Adam(lr=1e-3, decay=1e-6)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=adam_opt,
+                  metrics=['accuracy'])
+
+    if data_augmentation:
+        # this is the augmentation configuration we will use for training
+        train_datagen = ImageDataGenerator(
+            rescale=1. / 255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+    else:
+        train_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(train_data_dir,
+                                                        target_size=(img_width, img_height),
+                                                        batch_size=batch_size
+                                                        )
+    val_generator = val_datagen.flow_from_directory(val_data_dir,
+                                                    target_size=(img_width, img_height),
+                                                    batch_size=batch_size
+                                                    )
+
+    model.fit_generator(train_generator,
+                        steps_per_epoch=nb_train_samples // batch_size,
+                        epochs=epochs,
+                        validation_data=val_generator,
+                        validation_steps=nb_validation_samples//batch_size,
+                        )
+
+
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = keras.applications.VGG16(include_top=False, weights='imagenet')
+
+    # class_mode = None to make our generator only yield batches of data, no labels
+    # this is necessary because predict_generator need this kind of generator (only data)
+    generator = datagen.flow_from_directory(train_data_dir,
+                                            target_size=(img_width, img_height),
+                                            batch_size=batch_size,
+                                            classes=classes,
+                                            class_mode=None,
+                                            shuffle=False)
+    bottleneck_features_train = model.predict_generator(generator, nb_train_samples // batch_size, verbose=1)
+    np.save('bottleneck_features_train.npy', bottleneck_features_train)
+
+    # the label is easy to recreate once we don't use shuffle above
+    bottleneck_label_train = np.array([i // NB_TRAIN_PER_CLASS for i in range(nb_train_samples)])
+    np.save('bottleneck_label_train.npy', bottleneck_label_train)
+
+    generator = datagen.flow_from_directory(val_data_dir,
+                                            target_size=(img_width, img_height),
+                                            batch_size=batch_size,
+                                            classes=classes,
+                                            class_mode=None,
+                                            shuffle=False)
+
+    bottleneck_features_validation = model.predict_generator(generator, nb_validation_samples // batch_size, verbose=1)
+    np.save('bottleneck_features_validation.npy', bottleneck_features_validation)
+
+    # again, recreating label is easy if we don't use shuffle above
+    bottleneck_label_val = np.array([i // NB_VAL_PER_CLASS for i in range(nb_validation_samples)])
+    np.save('bottleneck_label_val.npy', bottleneck_label_val)
+
+
+def add_fc_model():
+    train_data = np.load('bottleneck_features_train.npy')
+    train_label = keras.utils.to_categorical(np.load('bottleneck_label_train.npy'), num_classes=num_classes)
+    val_data = np.load('bottleneck_features_validation.npy')
+    val_label = keras.utils.to_categorical(np.load('bottleneck_label_val.npy'), num_classes=num_classes)
+
+    fc_model = Sequential()
+    fc_model.add(Flatten(input_shape=train_data.shape[1:]))
+    fc_model.add(Dense(256, activation='relu'))
+    fc_model.add(Dropout(0.5))
+    fc_model.add(Dense(num_classes))
+    fc_model.add(Activation('softmax'))
+    adam_opt = keras.optimizers.Adam(lr=1e-4, decay=1e-6)
+
+    fc_model.compile(loss='categorical_crossentropy', optimizer=adam_opt, metrics=['accuracy'])
+    fc_model.fit(train_data, train_label, epochs=1, batch_size=batch_size, validation_data=(val_data, val_label))
+    fc_model.save('bottleneck_fc_model.h5')
+
+save_bottlebeck_features()
+
+
