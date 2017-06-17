9e270fd66da60fa00869597861d259a0b18a7350
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-06-17 18:52:56 +0100

    remove saying text before taking photo

--- cozmo_image.py
+++ cozmo_image.py
@@ -1,7 +1,7 @@
 import cozmo
 import time
 from scipy.misc import imread, imresize, imsave, imshow
-
+import os
 
 def cozmo_program(robot: cozmo.robot.Robot):
     robot.camera.image_stream_enabled = True  # setting this make it None
@@ -9,25 +9,37 @@ def cozmo_program(robot: cozmo.robot.Robot):
     robot.drive_off_charger_on_connect = False
     #time.sleep(4)
     print("enter exit to quit program, any other input will be considered ready")
-    cnt = 1
     path = "/home/hav/workplace/camcoz/"
+
+    # find max id images
+    im_names = sorted(os.listdir(path))
+    im_names = [e for e in im_names if 'JPEG' in e]
+    if len(im_names) > 0:
+        cur_max = int(im_names[-1][0:3])
+        cnt = cur_max + 1
+    else:
+        cnt = 1
+    print(cnt)
+
     while True:
         print("enter: ", end='')
         ready = input()
         if ready == "exit":
             break
-        cur_img_name = "im%02d.JPEG" % cnt
-        robot.say_text("take picture").wait_for_completed()
+        cur_img_name = "%03d.JPEG" % cnt
+        #robot.say_text("take picture").wait_for_completed()
         latest_img = robot.world.latest_image
         img = latest_img.raw_image
-        #img = imresize(img, (224, 224))
+        img = imresize(img, (224, 224))
         imsave(path + cur_img_name, img)
+        print("done %d" % cnt)
         cnt += 1
 
 
+
 # cozmo.run_program(cozmo_program, use_viewer=True)
-im1 = imread("/home/hav/workplace/camcoz/try2/im13_pen.JPEG")
-im2 = imread("/home/hav/workplace/camcoz/try2/im14_pen.JPEG")
-imshow(im1 - im2)
+# im1 = imread("/home/hav/workplace/camcoz/try2/im13_pen.JPEG")
+# im2 = imread("/home/hav/workplace/camcoz/try2/im14_pen.JPEG")
+# imshow(im1 - im2)
 
 
